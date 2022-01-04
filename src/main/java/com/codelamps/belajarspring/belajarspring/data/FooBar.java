bd5460374b17678a04b05b7d2e18fbf9843833f7
arzailants <andrerizaldib@gmail.com>
2022-01-04 16:18:52 +0700

    Menambah Kelas Belajar Dependency Injection di Spring

---
+++ src/main/java/com/codelamps/belajarspring/belajarspring/data/FooBar.java
@@ -0,0 +1,13 @@
+package com.codelamps.belajarspring.belajarspring.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class FooBar {
+
+    private Foo foo;
+
+    private Bar bar;
+}
