f4a7d517d518b2e36d2cea3d98cbc4568e4f08d4
arzailants <andrerizaldib@gmail.com>
2022-01-08 20:29:00 +0700

    Menambahkan Multiple Constructor, dengan @Autowired

--- src/main/java/com/codelamps/belajarspring/belajarspring/service/ProductService.java
+++ src/main/java/com/codelamps/belajarspring/belajarspring/service/ProductService.java
@@ -2,6 +2,7 @@ package com.codelamps.belajarspring.belajarspring.service;
 
 import com.codelamps.belajarspring.belajarspring.repository.ProdukRepository;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,12 @@ public class ProductService {
     @Getter
     private ProdukRepository produkRepository;
 
+    @Autowired
     public ProductService(ProdukRepository produkRepository){
+        this.produkRepository = produkRepository;
+    }
+
+    public ProductService(ProdukRepository produkRepository, String name){
         this.produkRepository = produkRepository;
     }
 }
